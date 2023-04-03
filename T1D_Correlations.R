75b481a98d595346dbb9df4fe3ddf529a469c26e
Reza Moosavi <98999623+RzaMsv@users.noreply.github.com>
2023-04-02 18:56:17 -0600

    Add files via upload

---
+++ T1D_Correlations.R
@@ -0,0 +1,14 @@
+library(Seurat)
+library(tidyverse)
+
+CD4 <- Read10X(data.dir = "Positive/")
+mergedNML <- CreateSeuratObject(counts = CD4, project = "T1D", min.cells = 3, min.features = 200)
+
+# now let's get rid of low quality cells
+mergedNML <- subset(mergedNML, subset = nFeature_RNA > 500 & nFeature_RNA <5000 & nCount_RNA < 20000)
+
+
+# Extract the gene expression matrix
+gene_exp <- as.matrix(GetAssayData(object = mergedNML, slot = "counts"))
+rownames(gene_exp)
+write.csv(gene_exp, file = "gene_expression_matrixP.csv", row.names = TRUE)
